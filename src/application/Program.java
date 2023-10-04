282a48131f9e556d33bc44e29f1e3f6e8ff14130
Caio Bruno <caiobruno90@yahoo.com>
2023-10-04 13:26:23 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entitites.Department;
+import model.entitites.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "BOb", "bob@gmail.com", new Date(), 3000.00, obj);
+		
+		System.out.println(seller);
 	}
 
 }
