059a1b9c61965549506479e07d1ed9e2fad7b1ea
Caio Bruno <caiobruno90@yahoo.com>
2023-10-05 13:23:14 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,10 +31,17 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
 		System.out.println("\n=== TEST 4: seller SellerInsert =====");
 		Seller  newSeller = new Seller(null , "Greg", "Greg@greg.com.br", new Date() , 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted new id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller SellerUpdate =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Wayne");
+		sellerDao.update(seller);
+		System.out.println("update completed");
 	}
 
 }
